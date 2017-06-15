6b9e6087a1457922519ab1748bddd70560e9723c
jiro.kirameki <yukiko-teranishi@dac.co.jp>
2017-06-15 00:52:06 +0000

    DRY

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,57 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      flash[:success] = 'task が正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'task が投稿されませんでした'
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      flash[:success] = 'task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'task は更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+
+    flash[:success] = 'task は正常に削除されました'
+    redirect_to task_url
+  end
+  
+  private
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content)
+  end
+  
+end
